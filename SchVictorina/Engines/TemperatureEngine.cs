8b17bd6496fd8ec56cd308e32df0f46b0a518515
Boris E. Moiseev <bmoiseev@dataeast.com>
2022-01-15 14:50:56 +0700

    Sorting...

--- SchVictorina/Engines/TemperatureEngine.cs
+++ SchVictorina/Engines/TemperatureEngine.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.Net;
+using System.Xml.Linq;
 using System.Collections.Generic;
-using System.Text;
+using SchVictorina.Utilites;
+using System.Globalization;
 
 namespace SchVictorina.Engines
 {
-    class TemperatureEngine
+    internal class TemperatureEngine : BaseEngine
     {
+        private static double? GetActualTemperature(int stationId)
+        {
+            var xmlElement = XElement.Load($"https://meteoinfo.ru/rss/forecasts/index.php?s={stationId}");
+            var desc = xmlElement.Element("channel").Element("item").Element("description").Value;
+            var temperature = desc.FindSubstring("днём", "°");
+            return double.Parse(temperature, CultureInfo.InvariantCulture);
+        }
+
+        public override TaskInfo GenerateQuestion()
+        {
+            var nskTemperature = GetActualTemperature(29637); //Температура в Новосибирске
+            var iskTemperature = GetActualTemperature(29730); //Температура в Искитиме
+            var tlmTemperature = GetActualTemperature(29630); //Температура около Толмачёво
+            return new TaskInfo
+            {
+                Question = "Какая погода сейчас в новосибирске (в центре)?",
+                //IVQuestion = $"{nskTemperature}{iskTemperature}{tlmTemperature}",
+                AnswerOptions = new object[]
+                {
+                    tlmTemperature.ToString(),
+                    iskTemperature.ToString(),
+                    nskTemperature.ToString()
+                },
+                RightAnswer = nskTemperature.ToString()
+            };
+        }
+
     }
 }
---
+++ SchVictorina/Utilites/ConvertUtilites.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Telegram.Bot.Types.ReplyMarkups;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchVictorina.Utilites
+{
+    internal static class ConvertUtilites
+    {
+        public static string FindSubstring(this string text, string from, string to)
+        {
+            var startIndex = text.IndexOf(from);
+            var endIndex = text.IndexOf(to, startIndex);
+            startIndex += from.Length;
+            var result = text.Substring(startIndex, endIndex - startIndex);
+            return result;
+        }
+        public static InlineKeyboardMarkup FromAnswerOptionsToKeyboardMarkup(TaskInfo question)
+        {
+
+        } 
+    }
+}
