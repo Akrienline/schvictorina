38e45b2ad1e640fdef63d97f6e8e6b61cb713ae2
Alexey <alekami649@gmail.com>
2022-03-14 22:32:11 +0700

    +

--- SchVictorina.WebAPI/Controllers/TelegramProcessing.cs
+++ SchVictorina.WebAPI/Controllers/TelegramProcessing.cs
@@ -178,15 +178,9 @@ namespace SchVictorina.WebAPI.Controllers
                             {
                                 var result = functionButton.Class?.Invoke();
                                 if (result != null)
-                                {
-                                    if (result.ParseMode == null)
-                                        await botClient.SendTextAndImage(update, result.Text, result.ImagePath);
-                                    else if (result.ParseMode == ParseMode.Html)
-                                        await botClient.SendTextAndImageAsHTML(update, result.Text, result.ImagePath);
-                                    else if (result.ParseMode == ParseMode.Markdown || result.ParseMode == ParseMode.MarkdownV2)
-                                        await botClient.SendTextAndImageAsMarkdown(update, result.Text, result.ImagePath);
-                                }
+                                    await botClient.SendTextAndImage(update, result.Text, result.ImagePath);
                                 await GenerateButtonsAndSend(botClient, update, ButtonConfig.RootButton);
+                                return;
                             }
                         }
                     }
